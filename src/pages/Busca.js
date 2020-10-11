f3c1d6799d05cc8a82dbe87a2ec80feb106dcb33
JuniorMaran <33530325+JuniorMaran@users.noreply.github.com>
2020-10-11 00:18:53 -0300

    hooks

--- src/pages/Busca.js
+++ src/pages/Busca.js
@@ -1,102 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import mock from "../models/mocks"
 
-export default class Busca extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      medicamentos: []
-    }
+export default function Busca () {
+
+  const [medicamentos, setMedicamentos] = useState([]);
+  var [interacoes, setInteracoes] = useState([]);
 
 
-  }
-  redirectInteracao() {
+  function redirectInteracao(){
     window.location.href = '/interacao';
   }
-  redirectReceituario() {
+
+  function redirectReceituario (){
     window.location.href = '/receitas';
   }
 
-  getMedicamentos(medicamentos) {
-    //aqui receberá os medicamentos do input validados
-    // for(var i = 0; i <= medicamentos.length; i++){
-    //   console.log("i")
-    // }
 
-    // medicamentos.forEach(function (newItem) {
-    //   mock.drugs.forEach(function (item) {
-    //     item.interaction.forEach(function(interaction){
+  function enviarDados() {
+    interacoes = []
 
-    //       if (newItem === item.name) {
-    //         console.log("medicamentos: ", newItem)
-    //         console.log("nome: ", item.name)
-    //         console.log("interaction:",interaction)
-    //       }
-
-    //     })
-    //   })
-    // })
-  }
-
-  enviarDados() {
-    var medicamentos = this.state.medicamentos
-
-    if (medicamentos != "") {
+    if (medicamentos && medicamentos.length > 0 && medicamentos !== "") {
       var medicamentosLista = medicamentos.split(";")
-      var medInteractions
-      mock.drugs.forEach(function (item) {
-        medicamentosLista.forEach(function (medic) {
-                medInteractions.append(medic, item)
-                
-          })
-        })
-      // medicamentosLista.forEach(medic => 
-      // mock.drugs.forEach(item => 
 
-      //   medic === item.name ? medInteractions.push(item) : null)
-      // )
+      medicamentosLista.forEach(medic => mock.drugs.forEach(item => item.name === medic ? interacoes.push(item) : null));
 
-      // medicamentosLista.forEach(item => {
-      //     medInteractions = mock.drugs.push(medic => medic.name === item);
-      // });
+      setInteracoes(interacoes);
 
+      console.log("medInteractions", interacoes)
 
-      console.log("medInteractions", medInteractions)
-      //this.getMedicamentos(medicamentosLista)
+      redirectInteracao()
+
     }
 
     //validar aqui se o medicamento está na api
 
     else
       console.log("insira os medicamentos")
+      
+      
+
   }
 
-  handleChange = event => {
-    this.setState({ medicamentos: event.target.value });
+  function handleChange ( event ){
+    setMedicamentos(event.target.value)
   };
 
-  render() {
+    
     return (
-      <section className="containerBusca">
 
+      <section className="containerBusca">
         <img src="./../logo192.png" className="imagem" alt="imagem" />
         <p className="text_p1"> Insira o nome dos medicamentos </p>
         <div>
           <p className="text_p2"> Insira os nomes dos medicamentos, separados por ";" <br></br> (ponto e vírgula seguido por um espaço)</p>
-          <input className="input_busca" placeholder="paracetamol; bepantol" value={this.state.medicamentos} onChange={this.handleChange} />
+          <input className="input_busca" placeholder="paracetamol; bepantol" onChange={handleChange}  />
         </div>
         <div>
-          <button className="button" onClick={this.enviarDados.bind(this)}>
+          <button className="button" onClick={enviarDados}>
             Pesquisar
           </button>
         </div>
         <div>
-          <button className="button" onClick={this.redirectReceituario.bind(this)}>
+          <button className="button" onClick={redirectReceituario.bind()}  >
             Receituário
             </button>
         </div>
 
       </section>
     );
-  }
+  
 }
--- src/pages/Interacao.js
+++ src/pages/Interacao.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, { setState } from 'react';
 import ChordDiagram from 'react-chord-diagram';
 import BackButton from '../components/BackButton';
 
-export default class Interacao extends Component {
+export default function Interacao(){
+    
 
-  criarMatriz(){
+  function criarMatriz(){
     let qtdMedicamentos = 20
     const matriz = Array(qtdMedicamentos).fill(0);
 
     for (let i in matriz){
       matriz[i] = Array(qtdMedicamentos).fill(1);
     }
+    
+
     return matriz;
   }
 
-  exibirMedicamentos(){
+  function exibirMedicamentos(){
     let medicamentos = ['medicamento 1', 'medicamento 2', 'medicamento 3', 'medicamento 4','medicamento 5', 'medicamento 6', 'medicamento 7', 'medicamento 8', 'medicamento 9', 'medicamento 10', 'medicamento 11', 'medicamento 12','medicamento 13', 'medicamento 14', 'medicamento 15', 'medicamento 16', 'medicamento 17', 'medicamento 18', 'medicamento 19', 'medicamento 20']
 
     return medicamentos;
   }
 
-  selecionarCores(){
-    var qtdMedicamentos = this.exibirMedicamentos()
+  function selecionarCores(){
+    var qtdMedicamentos = exibirMedicamentos()
     const arrayCores = ["#FFA500", "#FFDD89", "#957244", "#F26223", "#FFEBCD", "#FFE4C4", "#FFDAB9", "#FFE4B5", "#87CEEB", '#B0C4DE',"#FFA500", "#FFDD89", "#957244", "#F26223", "#FFEBCD", "	#FFE4C4", "#FFDAB9", "#FFE4B5", "#87CEEB", '#B0C4DE']
     let cor = arrayCores.sort(() => 0,5- Math.random());
     let cores = cor.slice(0, qtdMedicamentos.length);
@@ -29,19 +32,18 @@ export default class Interacao extends Component {
     return cores
   }
 
-
-render() {
   return (
+    
       <section className = "alignGraph">
         <div>
           <ChordDiagram 
-          height={700}
-          width= {700}
-          outerRadius = {200}
-            matrix={this.criarMatriz()}
+            height={700}
+            width= {700}
+            outerRadius = {200}
+            matrix={criarMatriz()}
             componentId={1}
-            groupLabels={this.exibirMedicamentos()}
-            groupColors={this.selecionarCores()}
+            groupLabels={exibirMedicamentos()}
+            groupColors={selecionarCores()}
           />
         </div>
         <div className = "alignButton">
@@ -50,5 +52,5 @@ render() {
         </div>
       </section>
     );  
-  }
+  
 }
