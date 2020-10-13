ef115bf873e07497fe600a65a1b5685785f8a975
kleber92 <kleber.barilli73@gmail.com>
2020-10-13 14:13:15 -0300

    refatoração das rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Busca from './pages/Busca';
-import Receitas from './pages/Receitas';
-import Interacao from './pages/Interacao';
 import { Provider } from 'react-redux'
 import store from './store';
+
+import Routes from '../src/routes';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/busca" component={Busca} />
-          <Route path="/receitas" component={Receitas} />
-          <Route path="/interacao" component={Interacao} />
-        </Switch>
-      </BrowserRouter>
+      <Routes></Routes>
     </Provider>
   );
 }
